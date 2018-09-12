b6bc04a4c180f3ae3d5e13e6c86ba4c6bdc0535e
skyline75489 <skyline75489@outlook.com>
2018-09-12 14:59:40 +0800

    Assets

--- assets/app/show_answers.js
+++ assets/app/show_answers.js
@@ -5,13 +5,20 @@ $('.show-answers').on('click', function(){
             answer = $(".answer", item).text(),
             post = $(".post", item)[0],
             codeMirror = window.codeMirrors[cnt];
-            
-            $(code).val(answer);
-            codeMirror.setValue(answer);
-            codeMirror.save();
+
+        // Prevents the screen from freezing 
+        setTimeout(function(){
+            // Copy from code element if no answer is required for a lesson
+            if (answer === "") answer = $(code).text();
+            // Save code only if the lesson has any
+            if (codeMirror != null) {
+                codeMirror.setValue(answer);
+                codeMirror.save();
+            }
             item.style.visibility = "visible";
             if (post !== undefined) {
                 post.style.visibility = "visible";
             }
+        }, 1);
     });
 });
